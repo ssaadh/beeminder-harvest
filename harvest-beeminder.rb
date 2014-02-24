f09165578f775032969daa52f11bd0415dca7a49
iknowicouldalwaysturn <iknowicouldalwaysturn@gmail.com>
2014-02-24 17:43:02 -0500

    Use the methods created in harvest.rb and the info extracted from the text files to send time data from Harvest to Beeminder. The date is defaulted to yesterday. All code obviously for specific use case.

--- harvest-beeminder.rb
+++ harvest-beeminder.rb
@@ -2,5 +2,15 @@
 
 require_relative 'harvest'
 
+date = yesterday
+
 harvest_clients = file_contents( './harvest.txt' )
 beeminder_goals = file_contents( './beeminder.txt' )
+
+update_beeminder_goal( beeminder_goals[ 0 ], total_time, date )
+beeminder_goals.shift
+
+for i in 0..harvest_clients.count
+  harvest_time = client_time( harvest_clients[ 0 ], date )
+  update_beeminder_goal( beeminder_goals[ i ], harvest_time , date )
+end
