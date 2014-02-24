a7fa469bda74982e4be3b03e0ea6c8227711b0ca
iknowicouldalwaysturn <iknowicouldalwaysturn@gmail.com>
2014-02-24 17:40:34 -0500

    Have the Harvest clients and Beeminder goals in text files. Code extracts the info.

--- harvest-beeminder.rb
+++ harvest-beeminder.rb
@@ -1,1 +1,6 @@
 # Own code to push Harvest to Beeminder
+
+require_relative 'harvest'
+
+harvest_clients = file_contents( './harvest.txt' )
+beeminder_goals = file_contents( './beeminder.txt' )
--- harvest.rb
+++ harvest.rb
@@ -48,3 +48,12 @@ end
 def yesterday
   Time.now - 24 * 60 * 60
 end
+
+def file_contents( location )
+  an_array = Array.new
+  File.open location do | f |    
+    f.each_line { | line | an_array.push line.chomp }
+  end
+  
+  return an_array
+end
