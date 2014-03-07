48594a609e911d9d2e6e5a23ff6470082bdc156e
iknowicouldalwaysturn <iknowicouldalwaysturn@gmail.com>
2014-03-07 02:09:33 -0500

    Minor edits. Fixed file location of the basic text data for Harvest, Beeminder, fixed bug where loop went on one too many times, and typo/bug of always getting the first harvest client's time in the loop instead of the current loop number's. Also added basic puts so can at minimum know what beeminder goal code has been run.

--- code/harvest-beeminder.rb
+++ code/harvest-beeminder.rb
@@ -4,14 +4,19 @@ require_relative 'harvest'
 
 date = yesterday
 
-harvest_clients = file_contents( './data/harvest.txt' )
-beeminder_goals = file_contents( './data/beeminder.txt' )
+harvest_clients = file_contents "#{File.dirname(__FILE__)}/data/harvest.txt"
+beeminder_goals = file_contents "#{File.dirname(__FILE__)}/data/beeminder.txt"
 
 total = total_time( date )
-update_beeminder_goal( beeminder_goals[ 0 ], total, date )
+total = total - client_time( harvest_clients.last, date )
+
+update_beeminder_goal( beeminder_goals.first, total, date )
+puts "done #{beeminder_goals.first}"
 beeminder_goals.shift
 
-for i in 0..harvest_clients.count
-  harvest_time = client_time( harvest_clients[ 0 ], date )
-  update_beeminder_goal( beeminder_goals[ i ], harvest_time , date )
+count = harvest_clients.count - 1
+for i in 0..count
+  harvest_time = client_time( harvest_clients[ i ], date )
+  update_beeminder_goal( beeminder_goals[ i ], harvest_time, date )
+  puts "done #{beeminder_goals[ i ]}"
 end
