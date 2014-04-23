7867b74d6452ab263fd6fac43497737d82957499
iknowicouldalwaysturn <iknowicouldalwaysturn@gmail.com>
2014-04-23 16:28:48 -0400

    Forgot to git ignore .env
    - And single trailing whitespace removal

--- code/base.rb
+++ code/base.rb
@@ -10,7 +10,7 @@ end
 # returns each line from the text file as a single array element
 def file_contents( location )
   an_array = Array.new
-  File.open location do | f |    
+  File.open location do | f |
     f.each_line { | line | an_array.push line.chomp }
   end
   
